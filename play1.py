a2f724cb03371aecdaa3475af18ff2907ae4265b
sho221 <sho221@hanmail.net>
2021-03-18 14:41:20 +0900

    0318

---
+++ play1.py
@@ -0,0 +1,23 @@
+import json
+
+user = json.loads('{"id" :100, "name" : "홍길동"}')
+ 
+# print('username is ' + user['name'])
+
+print(f'username is {user["name"]}')
+
+def userinfo(u):
+    """사용자 정보를 상세히 보여줌
+
+    Args:
+        u (User): User Json Object
+ 
+    Returns:
+        String : User Information
+    """
+
+    id = u["id"]
+    name = u["name"]
+    return f'ID is {id}, Name is {name}'
+
+print(f'User information: {userinfo(user)}')
