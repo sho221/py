e0c9a3926573b1e85f2f91dd103766ac7d32b848
sho221 <sho221@hanmail.net>
2021-03-22 15:15:29 +0900

    add br1

--- gittest.py
+++ gittest.py
@@ -2,4 +2,6 @@ print("9999999999 for br1")
 
 print("9999999999 for br1")
 print("9999999999 for br1")
-print("9999999999 for br1")
+
+
+print("9999999999 for br1")print("9999999999 for br1")print("9999999999 for br1")
